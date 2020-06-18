2d556ac00049456f6cddb84c2e163a0f0fed1868
Eka Prasetia <ekaone3033@gmail.com>
2020-06-18 15:06:08 +0700

    Updates

---
+++ precache-manifest.d953a177c97a67172dac879ac40ffa9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f636167a2299d6f99f185b81d4942ab5",
+    "url": "/Anime-JS/index.html"
+  },
+  {
+    "revision": "a503d35c4ff64f7c0f3a",
+    "url": "/Anime-JS/static/css/main.901ec96a.chunk.css"
+  },
+  {
+    "revision": "4f0777a7832a0c39692d",
+    "url": "/Anime-JS/static/js/2.ec2baf99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Anime-JS/static/js/2.ec2baf99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a503d35c4ff64f7c0f3a",
+    "url": "/Anime-JS/static/js/main.dfbb790f.chunk.js"
+  },
+  {
+    "revision": "2aeefdb30c3543442cd8",
+    "url": "/Anime-JS/static/js/runtime-main.52b17286.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Anime-JS/static/media/logo.ee7cd8ed.svg"
+  }
+]);
